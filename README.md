bcae9fbfd3a90f7abfcaefbe5a9f4d973f54c031
amrgharz <ammor.ghr@gmail.com>
2021-03-06 10:40:10 +0400

    first 16 lesture assignments

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS course assignments
+This is a repo where I will be adding every assignments I sovle through out the course
---
+++ assignments.js
@@ -0,0 +1,33 @@
+//Values and variables lecture
+
+let country = "Syria";
+let continent = 'Asia';
+let population = 18;
+
+console.log(country, continent, population)
+
+console.log(typeof null)
+
+//Data Types lecture assignments
+
+let isIsland  = false;
+let language;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language)
+
+//Let, const, var lecture assignments
+language = "Arabic";
+console.log(language)
+
+//Basic operator lecture assignments
+let halfThePopulation = population / 2;
+console.log(++population);
+let finlandPopulation = 6;
+console.log( population > finlandPopulation)
+let avaragePopulation = 33;
+console.log(population < avaragePopulation)
+let description = "Portugal is in Europe, and its 11 million people speak portuguese"
+console.log(description)
+
+//Strings and Template Literals Lecture
+description = `Portugal is in Europe, and its 11 million speak portugese`
